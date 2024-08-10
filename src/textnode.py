9d3913ab70abb6c09bd239493ad7e5669612a833
hectormilla <37377168+HectorMilla@users.noreply.github.com>
2024-08-09 23:55:40 -0400

    created textnode class in textnode.py

--- src/textnode.py
+++ src/textnode.py
@@ -1,1 +1,10 @@
 
+class Textnode:
+    def __init__(self, text, text_type,url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __eq__(self, text_node2):
+        if self.text == text_node2.text and self.text_type == text_node2.text_type and self.url == text_node2.url:
+            return True
