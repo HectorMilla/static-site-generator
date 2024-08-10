cb92e1562f5bfecab22a6f52e815e408e51203b1
hectormilla <37377168+HectorMilla@users.noreply.github.com>
2024-08-10 00:32:24 -0400

    added eq and repr methods to textnode class

--- src/main.py
+++ src/main.py
@@ -1,1 +1,3 @@
-print("hello world")
+from textnode import main
+
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,6 @@
 
 class Textnode:
-    def __init__(self, text, text_type,url):
+    def __init__(self, text, text_type,url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -8,3 +8,12 @@ class Textnode:
     def __eq__(self, text_node2):
         if self.text == text_node2.text and self.text_type == text_node2.text_type and self.url == text_node2.url:
             return True
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+
+def main():
+    test = Textnode("This is a text node","bold","https://www.boot.dev")
+    print(test)
+
+main()  
