31070c365473845a5e9caba48731bcbddc10d37e
HectorMilla <37377168+HectorMilla@users.noreply.github.com>
2024-08-11 00:24:30 -0400

    added more unit test for html node and created LeafNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,7 +11,13 @@ class HTMLNode:
     def props_to_html(self):
         html_props_str = ""
         for prop in self.props:
-            html_props_str += f" {prop}: {self.props[prop]} "
+            html_props_str += f' {prop}: "{self.props[prop]}" '
+        return html_props_str
 
     def __repr__(self) -> str:
         return f"tag={self.tag} value={self.value} children={self.children} props={self.props}"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag=None, props=None):
+        super().__init__(value, tag, props)
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -9,7 +9,19 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode(
             "h1", "This is a test", "p", {"href": "www.google.com", "target": "_blank"}
         )
-        node.props_to_html()
+        self.assertEqual(
+            node.props_to_html(), ' href: "www.google.com"  target: "_blank" '
+        )
+
+    def test_none(self):
+        node = HTMLNode()
+        print(node, "none class works!")
+
+    def test_repr(self):
+        node = HTMLNode(
+            "h1", "This is a test", "p", {"href": "www.google.com", "target": "_blank"}
+        )
+        print(node.__repr__(), "| repr works")
 
 
 if __name__ == "__main__":
