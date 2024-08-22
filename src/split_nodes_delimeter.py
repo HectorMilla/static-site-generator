a65e53ca56bb7463eaa0729ad006fd8d8425c1c4
hectormilla <37377168+HectorMilla@users.noreply.github.com>
2024-08-22 00:18:40 -0400

    created split_nodes_delimeter and started writing the delimiter splitting function, added unit test for this function

---
+++ src/split_nodes_delimeter.py
@@ -0,0 +1,5 @@
+# function to split markdown into nodes
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -2,7 +2,7 @@ import unittest
 
 from textnode import TextNode
 from htmlnode import text_node_to_html_node
-
+from split_nodes_delimeter import split_nodes_delimiter
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", "bold")
@@ -20,10 +20,18 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node","italic","https://boot.dev")
         node2 = TextNode("This is a text node", "bold","https://boot.dev")
         self.assertNotEqual(node,node2)
-    #TRYING TO FIX OUTPUT OF TEST
+    
     def test_text_node_to_html_node(self):
         node = TextNode("This is a text node","italic","https://boot.dev")
         node2 = TextNode("This is a text node","italic")
         self.assertEqual(text_node_to_html_node(node2).to_html(),"<i>This is a text node</i>")
+    
+    def test_split_nodes_delimiter(self):
+        node = TextNode("This is text with a `code block` word", "text")
+        self.assertEqual(split_nodes_delimiter([node],"`", "code"),[
+    TextNode("This is text with a ", "text"),
+    TextNode("code block", "code"),
+    TextNode(" word", "text"),
+])
 if __name__ == "__main__":
     unittest.main()
