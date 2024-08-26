c8fde7265095988d2269c7642f985768ac108faa
hectormilla <37377168+HectorMilla@users.noreply.github.com>
2024-08-25 22:39:35 -0400

    added function to split images and links from text node

--- src/split_nodes_delimeter.py
+++ src/split_nodes_delimeter.py
@@ -5,6 +5,8 @@ from textnode import (
     text_type_bold,
     text_type_italic,
     text_type_code,
+    text_type_link,
+    text_type_image,
 )
 
 import re
@@ -34,12 +36,68 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
 #extract Links
 def extract_markdown_images(text):
+    pattern = r"!\[(.*?)\]\((.*?)\)"
+    matches = re.findall(pattern, text)
+    return matches
 
-    if re.findall(r"!\[(.*?)\]\((.*?)\)",text):
-        images = re.findall(r"!\[(.*?)\]\((.*?)\)",text)
-        return images
-    if re.findall(r"!\[(.*?)\]\((.*?)\)",text): 
-        links = re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)",text)
-        return links
 
+def extract_markdown_links(text):
+    pattern = r"(?<!!)\[(.*?)\]\((.*?)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+
+#split images and links
     
+def split_nodes_image(old_nodes):
+    new_nodes = [] 
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+        original_text = node.text
+        extract = extract_markdown_images(original_text)
+        if len(extract) == 0:
+            new_nodes.append(node)
+            continue
+        for image in extract:
+            sections = original_text.split(f"![{image[0]}]({image[1]})",1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0],text_type_text))
+            new_nodes.append(TextNode(image[0],text_type_image, image[1],))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, text_type_text))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != text_type_text:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, link section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], text_type_text))
+            new_nodes.append(TextNode(link[0], text_type_link, link[1]))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, text_type_text))
+    return new_nodes
+
+        
+node = TextNode(
+    "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+    text_type_text,
+)
+split_nodes_image([node])
